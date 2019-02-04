1c47e97a62970ffe962c5cdfff6b5e0e1a32608d
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-04 13:20:57 +0200

    Delete comments

--- TestProject.TaskLibrary/Tasks/Lesson1/Task3/Circle.cs
+++ TestProject.TaskLibrary/Tasks/Lesson1/Task3/Circle.cs
@@ -4,20 +4,10 @@ using System.Text;
 
 namespace TestProject.TaskLibrary.Tasks.Lesson1.Task3
 {
-    class Circle
+    static class Circle
     {
         public const double PI = 3.14;
-        //private double radius;
-
-        //public double Radius
-        //{
-        //    get { return radius; }
-        //    set { radius = value; }
-        //}   
-        //public Circle(double r)
-        //{
-        //    Radius = r;
-        //}
+        
         public static double Length(double r)
         {
             return 2 * PI * r;
