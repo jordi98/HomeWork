28484248ccff3d3425bc0b7ab6be7397eae4d061
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-05 17:42:18 +0200

    add and done 2nd task of 2nd lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson2/Task2/Task.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson2.Task2
+{
+    public class Task : IRunnable
+    {
+        public void Run()
+        {
+            Console.WriteLine("------------------Lesson2 Task2-----------------------");
+            double width = 0, height = 0, x, y;
+            Console.Write("Input width: ");
+            while (width <= 0)
+            {
+                while (!Double.TryParse(Console.ReadLine(), out width))
+                {
+                    Console.WriteLine("Error");
+                }
+            }            
+            Console.Write("Input height: ");
+            while (height <= 0)
+            {
+                while (!Double.TryParse(Console.ReadLine(), out height))
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            Console.Write("Input x: ");
+            while (!Double.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine("Error");
+            }
+            Console.Write("Input y: ");
+            while (!Double.TryParse(Console.ReadLine(), out y))
+            {
+                Console.WriteLine("Error");
+            }
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = width;
+            rectangle.Height = height;
+            rectangle.X = x;
+            rectangle.Y = y;
+            Console.WriteLine($"Perimeter is {rectangle.Perimeter()}");
+            Console.WriteLine("--------------------------------------------------");
+
+        }
+    }
+}
