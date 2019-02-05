e65d5f99d94af761c0f8b2a4fe5e23a1cb2b7e5d
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-05 17:05:15 +0200

    Add struct Person

---
+++ TestProject.TaskLibrary/Tasks/Lesson2/Task1/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson2.Task1
+{
+    public struct Person
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public Person(string nam, string snam, int ag)
+        {
+            name = nam;
+            surname = snam;
+            age = ag;
+        }
+        public void MyMethod(int n)
+        {
+            if (n > 0)
+            {
+                if (age > n)
+                {
+                    Console.WriteLine($"{name} {surname} older than {n}");
+                }
+                else
+                    Console.WriteLine($"{name} {surname} younger than {n}");
+            }
+            else
+                Console.WriteLine("n must be > 0");
+        }
+    }
+}
