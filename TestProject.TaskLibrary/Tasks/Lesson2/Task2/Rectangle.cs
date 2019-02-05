b9d4051b23d689227ca2ba1ff94c30e5635442aa
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-05 17:41:42 +0200

    add and done struct Rectangle

---
+++ TestProject.TaskLibrary/Tasks/Lesson2/Task2/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson2.Task2
+{
+    public struct Rectangle : ISize, ICoordinates
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public double Perimeter()
+        {
+            return 2 * (Width + Height);
+        }
+    }
+}
