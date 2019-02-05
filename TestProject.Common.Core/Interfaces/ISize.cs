52b50f714a7d6538fdf5432b42967bc25c508ed1
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-05 17:41:09 +0200

    add interfaces

---
+++ TestProject.Common.Core/Interfaces/ISize.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.Common.Core.Interfaces
+{
+    public interface ISize
+    {
+        double Width { get; set; }
+        double Height { get; set; }
+    }
+}
