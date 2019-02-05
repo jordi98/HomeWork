0d6d237cf4e6d05628fe830e667fcf9a73fc44ac
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-05 19:20:44 +0200

    add and done 5th task of 2nd lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson2/Task5/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson2.Task5
+{
+    enum LongRange : long { Max = 9223372036854775807, Min = -9223372036854775808 }
+
+    public class Task : IRunnable
+    {
+        public void Run()
+        {
+            Console.WriteLine("------------------Lesson2 Task5-----------------------");
+            foreach (var item in Enum.GetValues(typeof(LongRange)))
+            {
+                Console.WriteLine($"{item} = {(long)item}");
+            }
+            Console.WriteLine("------------------------------------------------------");
+        }
+    }
+}
