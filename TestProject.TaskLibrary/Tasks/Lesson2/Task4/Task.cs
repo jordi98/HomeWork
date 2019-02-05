6b7a5936a6674acacf3f5a1154f9a410886ba212
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-05 19:14:09 +0200

    add and done 4th task of 2nd lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson2/Task4/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson2.Task4
+{    
+    public class Task : IRunnable
+    {
+        public void Run()
+        {
+            Console.WriteLine("------------------Lesson2 Task4-----------------------");
+            Extensions.Sort();
+            Console.WriteLine("------------------------------------------------------");
+        }
+    }
+}
