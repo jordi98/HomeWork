010bd62c3b1f572a85f7ecaf257cab1655f2f6fd
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-05 19:15:14 +0200

    add class Extension with enum

---
+++ TestProject.TaskLibrary/Tasks/Lesson2/Task4/Extension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson2.Task4
+{
+    enum Colors { Red = 5, Blue = 4, White = 6, Black = 3, Green = 1, Yellow = 2, Violet = 7 }
+
+    public static class Extensions
+    {
+        public static void Sort()
+        {
+            foreach (var item in Enum.GetValues(typeof(Colors)))
+            {
+                Console.WriteLine($"{item} = {(int)item}");
+            }
+        }
+    }
+}
