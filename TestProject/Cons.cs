bd019ebc2f36dff4bcc90abc68b847b48b4b26d7
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-06 12:41:30 +0200

    add new class Cons

---
+++ TestProject/Cons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject
+{
+    public class Cons : IConsole
+    {
+        public void WriteLine(string str)
+        {
+            Console.WriteLine(str);
+        }
+        public void Write(string str)
+        {
+            Console.Write(str);
+        }
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
