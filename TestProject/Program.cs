5be67fb959873687da00f49ebe461c522c378223
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-06 19:24:14 +0200

    run 2nd task of 3rd lesson

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -12,7 +12,7 @@ namespace TestProject
             var tasks = new IRunnable[]
             {
                 //new TaskLibrary.Tasks.Lesson1.Task1.Task1(),
-                new TaskLibrary.Tasks.Lesson3.Task1.Task()
+                new TaskLibrary.Tasks.Lesson3.Task2.Task()
             };
 
             foreach (var task in tasks)
