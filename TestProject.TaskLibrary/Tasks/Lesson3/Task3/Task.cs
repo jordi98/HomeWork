8f42056663ae5bf77e4eb07e20d5fef20c549d4a
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-06 19:45:04 +0200

    update 3rd task of 3rd lesson

--- TestProject.TaskLibrary/Tasks/Lesson3/Task3/Task.cs
+++ TestProject.TaskLibrary/Tasks/Lesson3/Task3/Task.cs
@@ -9,12 +9,45 @@ namespace TestProject.TaskLibrary.Tasks.Lesson3.Task3
     {
         public void Run(IConsole console)
         {
-            Figure figure = new Figure(1, 2);
+            console.WriteLine("------------------Lesson3 Task3-----------------------");
+            int rectX, rectY, sqX, sqY, figX, figY;
+            console.Write("Input X for figure: ");
+            while (!Int32.TryParse(console.ReadLine(), out figX))
+            {
+                console.WriteLine("Error");
+            }
+            console.Write("Input Y for figure: ");
+            while (!Int32.TryParse(console.ReadLine(), out figY))
+            {
+                console.WriteLine("Error");
+            }
+            Figure figure = new Figure(figX, figY);
             figure.Draw(console);
-            Rectangle rectangle = new Rectangle(3, 4);
+            console.Write("Input X for rectangle: ");
+            while (!Int32.TryParse(console.ReadLine(), out rectX))
+            {
+                console.WriteLine("Error");
+            }
+            console.Write("Input Y for rectangle: ");
+            while (!Int32.TryParse(console.ReadLine(), out rectY))
+            {
+                console.WriteLine("Error");
+            }
+            Rectangle rectangle = new Rectangle(rectX, rectY);
             rectangle.Draw(console);
-            Square square = new Square(5, 6);
+            console.Write("Input X for square: ");
+            while (!Int32.TryParse(console.ReadLine(), out sqX))
+            {
+                console.WriteLine("Error");
+            }
+            console.Write("Input Y for square: ");
+            while (!Int32.TryParse(console.ReadLine(), out sqY))
+            {
+                console.WriteLine("Error");
+            }
+            Square square = new Square(sqX, sqY);
             square.Draw(console);
+            console.WriteLine("------------------------------------------------------");
         }
     }
 }
