7aa9d6038f27d4e8e3031ec69a15240024aad1b8
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-06 19:56:14 +0200

    add 4th task of 3rd lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson3/Task4/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson3.Task4
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+
+        }
+    }
+}
