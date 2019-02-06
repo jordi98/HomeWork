172ee331cbc55aa6af8599d1b7d806e5b747ba85
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-06 19:06:33 +0200

    add and done 1st task of 3rd lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson3/Task1/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson3.Task1
+{
+    public class Task: IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Draw(console);
+            Square square = new Square();
+            square.Draw(console);
+        }
+    }
+}
