2b5c90f44f2f3b174197fdea364bd18b4025aede
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-06 19:05:53 +0200

    add Rectangle and Square classes

---
+++ TestProject.TaskLibrary/Tasks/Lesson3/Task1/Square.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson3.Task1
+{
+    public class Square : Figure
+    {
+        public override void Draw(IConsole console)
+        {
+            Console.WriteLine("Class Square");
+        }
+    }
+}
