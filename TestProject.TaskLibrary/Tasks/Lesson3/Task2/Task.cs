89f004d2d1ef48164fdecbc26d27fe956f2eb2e2
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-06 19:11:28 +0200

    Revert "Revert "add abstract class Figure with ctor and auto-prop""
    This reverts commit 3a0d74c160e27fb20b4f9b2fe7fecbecf21b3449.

---
+++ TestProject.TaskLibrary/Tasks/Lesson3/Task2/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson3.Task2
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            Rectangle rectangle = new Rectangle(1, 2);
+            rectangle.Draw(console);
+            Square square = new Square(3, 4);
+            square.Draw(console);
+        }
+    }
+}
---
+++ TestProject.TaskLibrary/Tasks/Lesson3/Task3/Figure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson3.Task3
+{
+    public class Figure
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        
+        public Figure(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public virtual void Draw(IConsole console)
+        {
+            console.WriteLine($"Class Figure {X} {Y}");
+        }
+    }
+}
