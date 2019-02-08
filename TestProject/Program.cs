b1183e9a25ea5e0e28f5f4d8949b566e7dd7e0ee
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-08 13:10:00 +0200

    run arrays

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -13,7 +13,7 @@ namespace TestProject
             {
                 //new TaskLibrary.Tasks.Lesson1.Task1.Task1(),
                 //new TaskLibrary.Tasks.Lesson3.Task3.Task()
-                new TaskLibrary.Udemy.ForLoop.Task()
+                new TaskLibrary.Udemy.Arrays.Task()
             };
 
             foreach (var task in tasks)
