062fc9f2c5cf1c05603b29710c2c1becc07b3094
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-08 13:08:23 +0200

    run data types

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -13,7 +13,7 @@ namespace TestProject
             {
                 //new TaskLibrary.Tasks.Lesson1.Task1.Task1(),
                 //new TaskLibrary.Tasks.Lesson3.Task3.Task()
-                new TaskLibrary.Udemy.BasicExercices.Task()
+                new TaskLibrary.Udemy.DataTypes.Task()
             };
 
             foreach (var task in tasks)
