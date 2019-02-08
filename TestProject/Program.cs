9030ec36465b23cbaa023b871e43bd347a46e49e
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-08 13:09:40 +0200

    run for loop

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -13,7 +13,7 @@ namespace TestProject
             {
                 //new TaskLibrary.Tasks.Lesson1.Task1.Task1(),
                 //new TaskLibrary.Tasks.Lesson3.Task3.Task()
-                new TaskLibrary.Udemy.ConditionalStatements.Task()
+                new TaskLibrary.Udemy.ForLoop.Task()
             };
 
             foreach (var task in tasks)
