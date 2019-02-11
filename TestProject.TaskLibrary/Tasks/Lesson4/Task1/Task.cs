9ee4b141490833d4f6a89ae539efdce752bff501
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-11 19:11:58 +0200

    add 1st task of 4th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson4/Task1/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson4.Task1
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            console.WriteLine("------------------Lesson4 Task1-----------------------");
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            int[] array2 = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                array2[i] = (int)Math.Pow(array[i], 3);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                console.Write($"{array2[i]} ");
+            }
+            console.WriteLine("\n------------------------------------------------------");
+        }
+    }
+}
