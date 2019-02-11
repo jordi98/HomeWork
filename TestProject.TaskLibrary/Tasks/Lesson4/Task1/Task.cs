626e31362bbf13c8fbb2f416b13c0a6ab61baa07
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-11 20:51:25 +0200

    done 1st task of 4th lesson

--- TestProject.TaskLibrary/Tasks/Lesson4/Task1/Task.cs
+++ TestProject.TaskLibrary/Tasks/Lesson4/Task1/Task.cs
@@ -10,14 +10,31 @@ namespace TestProject.TaskLibrary.Tasks.Lesson4.Task1
         public void Run(IConsole console)
         {
             console.WriteLine("------------------Lesson4 Task1-----------------------");
-            int[] array = new int[5] { 1, 2, 3, 4, 5 };
-            int[] array2 = new int[5];
-            for (int i = 0; i < 5; i++)
+            int n;
+            console.Write("Input array dim: ");
+            while (!Int32.TryParse(console.ReadLine(), out n))
             {
-                array2[i] = (int)Math.Pow(array[i], 3);
+                console.WriteLine("Error");
             }
-            for (int i = 0; i < 5; i++)
+            int[] array = new int[n];
+            int[] array2 = array;
+            for (int i = 0; i < n; i++)
             {
+                console.Write($"Array[{i}]: ");
+                while (!Int32.TryParse(console.ReadLine(), out array[i]))
+                {
+                    console.WriteLine("Error");
+                }
+            }
+            console.Write("Your array: ");
+            for (int i = 0; i < n; i++)
+            {
+                console.Write($"{array[i]} ");
+            }
+            console.Write("\nNew array: ");
+            for (int i = 0; i < n; i++)
+            {
+                array2[i] = array[i] * array[i] * array[i];
                 console.Write($"{array2[i]} ");
             }
             console.WriteLine("\n------------------------------------------------------");
