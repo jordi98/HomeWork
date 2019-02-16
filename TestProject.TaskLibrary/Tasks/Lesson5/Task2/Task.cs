72e250d877611e9629b9958ec0888b6eeab3b046
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-16 17:37:11 +0200

    done 2nd task of 5th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson5/Task2/Task.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson5.Task2
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            console.WriteLine("------------------Lesson5 Task2-----------------------");
+            var list = new List<Person>();
+            list.Add(new Person { Name = "Andy", Age = 22, PhoneNumbers = new string[] { "111", "222", "333", "2735" } });
+            list.Add(new Person { Name = "Bruce", Age = 19, PhoneNumbers = new string[] { "444", "442", "553", "1346" } });
+            list.Add(new Person { Name = "Collin", Age = 16, PhoneNumbers = new string[] { "166", "277", "838" } });
+            list.Add(new Person { Name = "Danny", Age = 25, PhoneNumbers = new string[] { "199", "929", "963", "852", "179" } });
+            list.Add(new Person { Name = "Ethan", Age = 36, PhoneNumbers = new string[] { "1234", "252", "563" } });
+            list.Add(new Person { Name = "Facundo", Age = 27, PhoneNumbers = new string[] { "135", "4552", "342" } });
+            Person[] people = new Person[2];
+            people[0] = new Person { Name = "George", Age = 20, PhoneNumbers = new string[] { "1476", "1685", "9826" } };
+            people[1] = new Person { Name = "Helga", Age = 30, PhoneNumbers = new string[] { "246", "292", "97169", "9593" } };
+            list.AddRange(people);
+            foreach (var item in list)
+            {
+                console.Write($"\nName: {item.Name}\tAge: {item.Age}\t Phone numbers: ");
+                foreach (var number in item.PhoneNumbers)
+                {
+                    console.Write($"{number} ");
+                }
+            }
+            console.WriteLine("\n------------------------------------------------------");
+        }
+    }
+}
