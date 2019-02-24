44dc6a3d93b2a3ac3b5f888e3a6b9b4074adf015
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-24 22:11:40 +0200

    inserted class axis to lesson6 folder

---
+++ TestProject.TaskLibrary/Tasks/Lesson6/Axis.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson6
+{
+    internal static class Axis
+    {
+        public static void Draw(IConsole console, char[,] axis)
+        {
+            for (int y = 0; y < 41; y++)
+            {
+                for (int x = 0; x < 82; x++)
+                {
+                    console.Write($"{axis[y, x]}");
+                }
+
+                console.WriteLine(string.Empty);
+            }
+        }
+
+        public static void Set(char[,] axis)
+        {
+            for (int y = 0; y < 41; y++)
+            {
+                for (int x = 0; x < 82; x++)
+                {
+                    axis[y, x] = ' ';
+                    if ((y == 20 && x % 2 != 0) || (x == 41))
+                    {
+                        axis[y, x] = '+';
+                    }
+
+                    if (y == 20 && x == 41)
+                    {
+                        axis[y, x] = '0';
+                    }
+                }
+            }
+        }
+    }
+}
