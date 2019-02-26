1dd022025fd6480c6ca74990667676728db98a80
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-02-26 21:11:58 +0200

    added class Person to 7th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson7/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson7
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public IEnumerable<string> PhoneNumbers { get; set; }
+    }
+}
