a59705cfe2f8aa700fb7ce71fd565eb49fb0e46f
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-01 17:01:45 +0200

    added interface IDraw

---
+++ TestProject.Common.Core/Interfaces/IDraw.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.Common.Core.Interfaces
+{
+    public interface IDraw
+    {
+        void Draw(IConsole console, char[,] axis);
+    }
+}
