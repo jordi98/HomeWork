78c9ce426480252d1c33ec06c49d62acc460ab0d
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-06 21:58:45 +0200

    done 2nd task of 10th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson10/Task2/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson10.Task2
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            console.WriteLine("------------------Lesson10 Task2-----------------------");
+            this.SomeMethod();
+            console.WriteLine("------------------------------------------------------");
+        }
+
+        private void SomeMethod()
+        {
+            int[] values1 = { 1, 2, 3, 4, 5 };
+            int[] values2 = new int[4];
+            values2[values1.Length - 1] = values1[values1.Length - 1];
+        }
+    }
+}
