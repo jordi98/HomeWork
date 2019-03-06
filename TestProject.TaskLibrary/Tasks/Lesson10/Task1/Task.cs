d0b939d3ca6bede43b6d424ea9a71d5769d06ffa
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-06 21:58:22 +0200

    done 1st task of 10th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson10/Task1/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson10.Task1
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            console.WriteLine("------------------Lesson10 Task1-----------------------");
+            int i = RecursiveMethod();
+            console.WriteLine("------------------------------------------------------");
+        }
+
+        private static int RecursiveMethod()
+        {
+            return RecursiveMethod();
+        }
+    }
+}
