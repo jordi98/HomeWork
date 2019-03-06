032dc5de0d04f78eebdd0d44a36c63bf12c541a9
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-06 21:59:40 +0200

    done 5th task of 10th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson10/Task5/Task.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson10.Task5
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            console.WriteLine("------------------Lesson10 Task5-----------------------");
+            int a, b;
+            console.Write("Input a: ");
+            while (!int.TryParse(console.ReadLine(), out a))
+            {
+                console.WriteLine("Error");
+            }
+
+            console.Write("Input b: ");
+            while (!int.TryParse(console.ReadLine(), out b))
+            {
+                console.WriteLine("Error");
+            }
+
+            try
+            {
+                this.DoSomeMath(a, b);
+            }
+            catch (ArgumentException e)
+            when (e.ParamName == "a")
+            {
+                console.WriteLine($"{e.Message}");
+            }
+            catch (ArgumentException e)
+            when (e.ParamName == "b")
+            {
+                console.WriteLine($"{e.Message}");
+            }
+
+            console.WriteLine("------------------------------------------------------");
+        }
+
+        private void DoSomeMath(int a, int b)
+        {
+            if (a < 0)
+            {
+                ArgumentException argEx = new ArgumentException("Parameter should be greater than 0", "a");
+                throw argEx;
+            }
+
+            if (b > 0)
+            {
+                ArgumentException argEx = new ArgumentException("Parameter should be less than 0", "b");
+                throw argEx;
+            }
+        }
+    }
+}
