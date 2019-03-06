81f39be38fa19391eb57a24673622ac3481b9e9c
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-06 21:59:23 +0200

    done 4th task of 10th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson10/Task4/Task.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson10.Task4
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            console.WriteLine("------------------Lesson10 Task4-----------------------");
+            try
+            {
+                this.SomeMethod();
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                console.WriteLine($"{e.Message}");
+            }
+            catch (SystemException e)
+            {
+                console.WriteLine($"{e.Message}");
+                throw;
+            }
+            catch (Exception e)
+            {
+                console.WriteLine($"{e.Message}");
+                throw;
+            }
+
+            try
+            {
+                int i = RecursiveMethod();
+            }
+            catch (StackOverflowException e)
+            {
+                console.WriteLine($"{e.Message}");
+            }
+            catch (SystemException e)
+            {
+                console.WriteLine($"{e.Message}");
+                throw;
+            }
+            catch (Exception e)
+            {
+                console.WriteLine($"{e.Message}");
+                throw;
+            }
+
+            console.WriteLine("------------------------------------------------------");
+        }
+
+        private static int RecursiveMethod()
+        {
+            return RecursiveMethod();
+        }
+
+        private void SomeMethod()
+        {
+            int[] values1 = { 1, 2, 3, 4, 5 };
+            int[] values2 = new int[4];
+            values2[values1.Length - 1] = values1[values1.Length - 1];
+        }
+    }
+}
