8f824122ed11ef5bda7f355e5202bd72ab916735
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-18 20:28:24 +0200

    added class Car to 1st task of 13th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson13/Task1/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson13.Task1
+{
+    [DataContract]
+    [Serializable]
+    public class Car
+    {
+        [DataMember]
+        public int CarId;
+
+        [DataMember]
+        public decimal Price;
+
+        [DataMember]
+        public int Quantity;
+
+        [DataMember]
+        public decimal Total;
+
+        public Car()
+        {
+
+        }
+    }
+}
