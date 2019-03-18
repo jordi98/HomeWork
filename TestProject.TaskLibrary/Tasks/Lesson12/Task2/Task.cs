0ba3fbac399d26cceaa40a3d6d34e850f9daf4fd
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-18 20:27:21 +0200

    done 2nd task of 12th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson12/Task2/Task.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson12.Task2
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            console.WriteLine("------------------Lesson12 Task2-----------------------");
+            console.Write("Input name: ");
+            string name = console.ReadLine();
+            this.FindByName(console, "C:\\Users\\user\\Documents", name);
+            console.WriteLine("-------------------------------------------------------");
+        }
+
+        private void FindByName(IConsole console, string path, string partiallyName)
+        {
+            var dir = new DirectoryInfo(path);
+            var files = new List<FileInfo>();
+            try
+            {
+                files = dir.EnumerateFiles("*" + partiallyName + "*.txt").ToList();
+                foreach (var file in files)
+                {
+                    console.WriteLine(file.FullName);
+                }
+                
+            }
+            catch (UnauthorizedAccessException uAEx)
+            {
+                console.WriteLine(uAEx.Message);
+            }
+
+            if (!dir.Exists)
+            {
+                console.WriteLine("Dir does not exist");
+            }
+
+            try
+            {
+                foreach (var directory in dir.EnumerateDirectories("*"))
+                {
+                    FindByName(console, path + "\\" + directory.Name, partiallyName);
+                }
+            }
+            catch (UnauthorizedAccessException unAuthTop)
+            {
+                console.WriteLine($"\t{unAuthTop.Message}");
+            }
+            
+        }
+    }
+}
