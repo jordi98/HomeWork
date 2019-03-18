71903e0c9a2fb823979c62cdbb7989bf748a4420
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-18 20:26:34 +0200

    done 1st task of 12th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson12/Task1/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson12.Task1
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            console.WriteLine("------------------Lesson12 Task1-----------------------");
+            DirectoryInfo dir = new DirectoryInfo(@"d:\Yura");
+            console.WriteLine(dir.ToString());
+            this.ShowFiles(console, dir);
+            console.WriteLine("------------------------------------------------------");
+        }
+
+        private void ShowFiles(IConsole console, DirectoryInfo dir)
+        {
+            DirectoryInfo[] dirSubDirectories = dir.GetDirectories();
+            FileInfo[] dirFiles = dir.GetFiles();
+            foreach (DirectoryInfo d in dirSubDirectories)
+            {
+                console.WriteLine(d.ToString());
+                this.ShowFiles(console, d);
+            }
+
+            foreach (FileInfo f in dirFiles)
+            {
+                console.WriteLine(f.ToString());
+            }
+        }
+    }
+}
