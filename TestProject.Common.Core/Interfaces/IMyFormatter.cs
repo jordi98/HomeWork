59d916283da2d75dbd196f14f5e46950f7d389d0
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-18 20:29:40 +0200

    added IMyFormatter interface

---
+++ TestProject.Common.Core/Interfaces/IMyFormatter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TestProject.Common.Core.Interfaces
+{
+    public interface IMyFormatter
+    {
+        void Serialize(Stream serializationStream, object graph);
+
+        void Serialize(Stream serializationStream, object graph, Type type);
+
+        object Deserialize(Stream serializationStream);
+
+        object Deserialize(Stream serializationStream, Type type);
+
+        object ReadObject(Stream stream, Type type);
+
+        void WriteObject(Stream stream, object graph, Type type);
+    }
+}
