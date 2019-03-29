d80aeb9b13ce866437dc7bb0fa11f4c3666af500
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-30 00:40:18 +0200

    added HardCalculatorTest to NUnitTests

---
+++ NUnitTests/HardCalculatorTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using TestProject.TaskLibrary.Tasks.Lesson15.Task2;
+
+namespace Tests
+{
+    [TestFixture]
+    public class HardCalculatorTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            int[] a = { 1, 2, 3, 4 };
+            int[] b = { 4, 5, 6 };
+            int[] expected = { 5, 7, 9, 4 };
+            int[] actual = HardCalculator.Add(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SubTest()
+        {
+            int[] a = { 1, 2, 3 };
+            int[] b = { 4, 5, 6, 5 };
+            int[] expected = { -3, -3, -3, 5 };
+            int[] actual = HardCalculator.Sub(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MulTest()
+        {
+            int[] a = { 1, 2, 3 };
+            int[] b = { 4, 5, 6 };
+            int[] expected = { 4, 10, 18 };
+            int[] actual = HardCalculator.Mul(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void DivTest()
+        {
+            int[] a = { 4, 5, 6 };
+            int[] b = { 1, 2, 3 };
+            int[] expected = { 4, 2, 2 };
+            int[] actual = HardCalculator.Div(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
