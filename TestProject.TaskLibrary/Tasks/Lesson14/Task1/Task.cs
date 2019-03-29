d353f554039228d0f105ab302407ca57e62eb054
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-29 23:36:12 +0200

    done 1st task of 14th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson14/Task1/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson14.Task1
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            string first = "Ґанок";
+            string second = "Ґанок";
+            CultureInfo cultureInfo = new CultureInfo("uk-UA");
+            var areEqual = string.Equals(first, second, StringComparison.CurrentCulture);
+            console.WriteLine($"The two strings are equal - {areEqual}");
+        }
+    }
+}
