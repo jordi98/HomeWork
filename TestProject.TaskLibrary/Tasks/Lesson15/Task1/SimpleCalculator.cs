4d6183caa31fda0d11824d1b643e43a10e2b9691
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-03-30 00:25:15 +0200

    added SimpleCalculator class

---
+++ TestProject.TaskLibrary/Tasks/Lesson15/Task1/SimpleCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson15.Task1
+{
+    public static class SimpleCalculator
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
