62627b64ec41c80f332ff67b6fd313ac7ff1430c
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-04-02 20:40:01 +0300

    add ICalculator interface

---
+++ TestProject.Common.Core/Interfaces/ICalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject.Common.Core.Interfaces
+{
+    public interface ICalculator<T>
+    {
+        T Add(T num1, T num2);
+
+        T Sub(T num1, T num2);
+
+        T Mul(T num1, T num2);
+
+        T Div(T num1, T num2);
+    }
+}
