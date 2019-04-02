470e8ac9af95d27d227929896e12d370d20ab6a6
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-04-02 20:42:38 +0300

    update class SimpleCalculator. Now it implements ICalculator interface

--- TestProject.TaskLibrary/Tasks/Lesson15/Task1/SimpleCalculator.cs
+++ TestProject.TaskLibrary/Tasks/Lesson15/Task1/SimpleCalculator.cs
@@ -1,27 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TestProject.Common.Core.Interfaces;
 
 namespace TestProject.TaskLibrary.Tasks.Lesson15.Task1
 {
-    public static class SimpleCalculator
+    public class SimpleCalculator : ICalculator<int>
     {
-        public static int Add(int num1, int num2)
+        public int Add(int num1, int num2)
         {
             return num1 + num2;
         }
 
-        public static int Sub(int num1, int num2)
+        public int Sub(int num1, int num2)
         {
             return num1 - num2;
         }
 
-        public static int Mul(int num1, int num2)
+        public int Mul(int num1, int num2)
         {
             return num1 * num2;
         }
 
-        public static int Div(int num1, int num2)
+        public int Div(int num1, int num2)
         {
             return num1 / num2;
         }
