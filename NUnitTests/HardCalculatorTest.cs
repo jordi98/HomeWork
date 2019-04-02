53f8d8f7957fd271e4b3a47156c515352b23e9e8
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-04-02 20:46:06 +0300

    update NUnit tests

--- NUnitTests/HardCalculatorTest.cs
+++ NUnitTests/HardCalculatorTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using TestProject.Common.Core.Interfaces;
 using TestProject.TaskLibrary.Tasks.Lesson15.Task2;
 
 namespace Tests
@@ -6,13 +7,21 @@ namespace Tests
     [TestFixture]
     public class HardCalculatorTest
     {
+        private ICalculator<int[]> calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.calculator = new HardCalculator();
+        }
+
         [Test]
         public void AddTest()
         {
             int[] a = { 1, 2, 3, 4 };
             int[] b = { 4, 5, 6 };
             int[] expected = { 5, 7, 9, 4 };
-            int[] actual = HardCalculator.Add(a, b);
+            int[] actual = this.calculator.Add(a, b);
             Assert.AreEqual(expected, actual);
         }
 
@@ -22,7 +31,7 @@ namespace Tests
             int[] a = { 1, 2, 3 };
             int[] b = { 4, 5, 6, 5 };
             int[] expected = { -3, -3, -3, 5 };
-            int[] actual = HardCalculator.Sub(a, b);
+            int[] actual = this.calculator.Sub(a, b);
             Assert.AreEqual(expected, actual);
         }
 
@@ -32,7 +41,7 @@ namespace Tests
             int[] a = { 1, 2, 3 };
             int[] b = { 4, 5, 6 };
             int[] expected = { 4, 10, 18 };
-            int[] actual = HardCalculator.Mul(a, b);
+            int[] actual = this.calculator.Mul(a, b);
             Assert.AreEqual(expected, actual);
         }
 
@@ -42,7 +51,7 @@ namespace Tests
             int[] a = { 4, 5, 6 };
             int[] b = { 1, 2, 3 };
             int[] expected = { 4, 2, 2 };
-            int[] actual = HardCalculator.Div(a, b);
+            int[] actual = this.calculator.Div(a, b);
             Assert.AreEqual(expected, actual);
         }
     }
--- NUnitTests/SimpleCalculatorTest.cs
+++ NUnitTests/SimpleCalculatorTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using TestProject.Common.Core.Interfaces;
 using TestProject.TaskLibrary.Tasks.Lesson15.Task1;
 
 namespace Tests
@@ -6,11 +7,19 @@ namespace Tests
     [TestFixture]
     public class SimpleCalculatorTest
     {
+        private ICalculator<int> calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.calculator = new SimpleCalculator();
+        }
+
         [Test]
         public void AddTest()
         {
             int a = 5, b = 5, expected = 10;
-            int actual = SimpleCalculator.Add(a, b);
+            int actual = this.calculator.Add(a, b);
             Assert.AreEqual(expected, actual);
         }
 
@@ -18,7 +27,7 @@ namespace Tests
         public void SubTest()
         {
             int a = 5, b = 5, expected = 0;
-            int actual = SimpleCalculator.Sub(a, b);
+            int actual = this.calculator.Sub(a, b);
             Assert.AreEqual(expected, actual);
         }
 
@@ -26,7 +35,7 @@ namespace Tests
         public void MulTest()
         {
             int a = 5, b = 5, expected = 25;
-            int actual = SimpleCalculator.Mul(a, b);
+            int actual = this.calculator.Mul(a, b);
             Assert.AreEqual(expected, actual);
         }
 
@@ -34,7 +43,7 @@ namespace Tests
         public void DivTest()
         {
             int a = 5, b = 5, expected = 1;
-            int actual = SimpleCalculator.Div(a, b);
+            int actual = this.calculator.Div(a, b);
             Assert.AreEqual(expected, actual);
         }
     }
