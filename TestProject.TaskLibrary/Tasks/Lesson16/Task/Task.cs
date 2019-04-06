873e87085c162fe14f58f5247842105d1de2f90a
Yuri Danchyshyn <yurij98.dan@gmail.com>
2019-04-06 19:59:22 +0300

    done 1st tast of 16th lesson

---
+++ TestProject.TaskLibrary/Tasks/Lesson16/Task/Task.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using TestProject.Common.Core.Interfaces;
+
+namespace TestProject.TaskLibrary.Tasks.Lesson16.Task
+{
+    public class Task : IRunnable
+    {
+        public void Run(IConsole console)
+        {
+            console.WriteLine("------------------Lesson16 Task1-----------------------");
+            Assembly assembly = Assembly.LoadFile(@"C:\Users\user\Desktop\Epam\.Net-Lab-master\VectorLibrary\bin\Debug\netcoreapp2.1\VectorLibrary.dll");
+            foreach (var item in assembly.GetModules(true))
+            {
+                console.WriteLine($"{item}");
+            }
+
+            foreach (var type in assembly.GetExportedTypes())
+            {
+                console.WriteLine($"{type}");
+                foreach (var member in type.GetMembers())
+                {
+                    console.WriteLine($"{member}");
+                    if (member.MemberType == MemberTypes.Method)
+                    {
+                        foreach (var parameterInfo in ((MethodInfo)member).GetParameters())
+                        {
+                            console.WriteLine($"{parameterInfo}");
+                        }
+                    }
+                }
+            }
+
+            console.WriteLine("-------------------------------------------------------");
+        }
+    }
+}
